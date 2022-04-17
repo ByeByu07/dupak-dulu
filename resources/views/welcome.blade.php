3d130d871ac3d3380d0bc864fd53ddf87a5f4894
ByeByu07 <muhammadbaharuddin08123@gmail.com>
2022-04-17 22:27:39 +0700

    sampai view welcome

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,34 @@
+@extends('layout.main')
+@section('content')
+<div class="row">
+    <div class="col-lg-12">
+        <div class="jumbotron bg-warning">
+            <h1 class="display-4">Hello, EveryOne!</h1>
+            <p class="lead">Jasa Tulis SKP-DUPAK</p>
+            <hr class="my-4">
+            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        </div>
+
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-6">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">DUPAK Bundle</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Buat DUPAK</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">SKP Bundle</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Buat SKP</a>
+        </div>
+      </div>
+    </div>
+  </div>
+@stop
